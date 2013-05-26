36a12a1bf479fd16d21386bc654b3d96edd95a08
trungdv1987@gmail.com <trungdv1987@gmail.com@bd780fdc-8b88-1c8c-29bf-7a8af08a867f>
2013-05-26 15:21:39 +0000

    fix some layout issues

--- trunk/Drumbeats/src/com/android/drumbeat/HomeRowAdapter.java
+++ trunk/Drumbeats/src/com/android/drumbeat/HomeRowAdapter.java
@@ -6,12 +6,10 @@ import java.util.HashMap;
 
 import android.app.Activity;
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class HomeRowAdapter extends BaseAdapter {
--- trunk/Drumbeats/src/com/android/drumbeat/TextAdapter.java
+++ trunk/Drumbeats/src/com/android/drumbeat/TextAdapter.java
@@ -3,13 +3,10 @@ package com.android.drumbeat;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class TextAdapter extends BaseAdapter {
