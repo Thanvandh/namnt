0f9abaabd77ace7e9692c71de3621dee324e8fa8
nguyenthanhnam.hut <nguyenthanhnam.hut@bd780fdc-8b88-1c8c-29bf-7a8af08a867f>
2014-02-17 14:36:56 +0000

    1. MO charging

---
+++ bluesea/sourcecode/WebApiBluesea/WebApiBluesea/Controllers/ReceiveMOController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebApiBluesea.Utils;
+
+namespace WebApiBluesea.Controllers
+{
+    public class ReceiveMOController : ApiController
+    {
+        // POST api/receivemo
+        //public string Post([FromBody]string value)
+        //{
+        //    Debug.WriteLine(value);
+        //    return "abc";
+        //}
+        readonly log4net.ILog logAll = log4net.LogManager.GetLogger("request_response");
+        readonly log4net.ILog logError = log4net.LogManager.GetLogger("error");
+        public HttpResponseMessage Post(HttpRequestMessage request)
+        {
+
+            var requestText = request.Content.ReadAsStringAsync().Result;
+            string ip = HttpRequestMessageHelper.GetClientIpAddress(request);
+            logAll.Info("request|" + ip + "|" + requestText);
+            string result = "0";
+            logAll.Info("response|" + requestText + "|" + result);
+            return new HttpResponseMessage() { Content = new StringContent(result) };
+
+        }
+
+
+    }
+}
---
+++ bluesea/sourcecode/WebApiBluesea/WebApiBluesea/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiBluesea.WebApiApplication" Language="C#" %>
